433b903bdbabf82c84226f74ed580a03f765df50
fangkun <fangkun@ideamake.cn>
2022-10-06 01:20:12 +0800

    chore: add vitest

--- tests/main.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "solid-js/web";
-
-function App() {
-  return <div>hello world</div>;
-}
-
-render(App, document.body);
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  test: {
+    globals: true,
+    setupFiles: ["setupTest.ts"],
+    environment: "jsdom",
+    deps: {
+      inline: [/solid-js/, /solid-testing-library/],
+    },
+    transformMode: {
+      web: [/\.tsx?$/],
+    },
+  },
+  resolve: {
+    conditions: ["development", "browser"],
+  },
 });
