16d1896661d914b1bac25d18275abda073485bb6
fangkun <fangkun@ideamake.cn>
2022-10-06 12:07:09 +0800

    chore: configure build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitest/config";
 import solidPlugin from "vite-plugin-solid";
+import typescript from "@rollup/plugin-typescript";
 
 export default defineConfig({
   plugins: [solidPlugin()],
@@ -16,5 +17,26 @@ export default defineConfig({
   },
   resolve: {
     conditions: ["development", "browser"],
+  },
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "index.js",
+      fileName: "index",
+      formats: ["es", "umd"],
+    },
+    rollupOptions: {
+      external: ["solidJs"],
+      plugins: [
+        typescript({
+          target: "es2015",
+          declaration: true,
+          declarationDir: "./dist/types",
+          exclude: ["node_modules/**", "tests/**/*"],
+          allowSyntheticDefaultImports: true,
+          sourceMap: false,
+        }),
+      ],
+    },
   },
 });
